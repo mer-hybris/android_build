4190612ab12c0441dd91e8f6269f2340a497bb8e
Zhuqiankun <zhuqk0617@thundersoft.com>
2015-03-13 14:03:26 +0800

    FP2-90: european language support
    adjust the languages list to support European languages.
    
    Change-Id: I43988611d4963ac67bbd371b5c414b50504fe13f

--- target/product/locales_full.mk
+++ target/product/locales_full.mk
@@ -1,3 +1,3 @@
-PRODUCT_LOCALES := en_US cs_CZ da_DK de_AT de_CH de_DE de_LI el_GR en_AU en_CA en_GB en_NZ en_SG eo_EU es_ES fr_CA fr_CH fr_BE fr_FR it_CH it_IT ja_JP ko_KR nb_NO nl_BE nl_NL pl_PL pt_PT ru_RU sv_SE tr_TR zh_CN zh_HK zh_TW am_ET hi_IN
+PRODUCT_LOCALES := en_US cs_CZ da_DK de_AT de_CH de_DE de_LI el_GR en_GB eo_EU fr_CA fr_CH fr_BE fr_FR it_CH it_IT nb_NO es_ES et_EE nl_NL pl_PL pt_PT ru_RU sv_SE tr_TR bg_BG ca_ES fi_FI hr_HR hu_HU lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA en_XA si_LK is_IS mk_MK eu_ES gl_ES nl_BE
 
-$(call inherit-product, build/target/product/languages_full.mk)
+#$(call inherit-product, build/target/product/languages_full.mk)
