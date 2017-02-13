969055934c7ed20f83b79194eff1c3b3b9214568
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2017-02-13 15:41:05 +0100

    (hybris) Pickup kernel and vendor builds
    Signed-off-by: Simonas Leleiva <simonas.leleiva@jollamobile.com>

--- core/main.mk
+++ core/main.mk
@@ -495,7 +495,7 @@ ifneq ($(dont_bother),true)
 # Include all of the makefiles in the system
 #
 
-subdir_makefile_dirs := abi bionic bootable build device external hardware hybris libcore system
+subdir_makefile_dirs := abi bionic bootable build device external hardware hybris kernel libcore system vendor
 
 # Can't use first-makefiles-under here because
 # --mindepth=2 makes the prunes not work.
